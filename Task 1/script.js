72a027e3675a09020eb2a9f2ecf951adc47bdcfd
Tomas <tomas_narba@yahoo.com>
2022-04-04 13:50:05 +0300

    pirma uzd

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,64 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+const kilosEl = document.getElementById("search");
+const divEl = document.getElementById("output");
+
+document.forms[0].addEventListener("submit", (event) => {
+  event.preventDefault();
+  const kilosValue = kilosEl.value;
+  //clear output
+  divEl.textContent = "";
+  //main function
+  validInput(kilosValue);
+});
+
+//separate functions for generating outputs=======================================
+function outputIntro(kilos) {
+  const outputIntroEl = document.createElement("h4");
+  outputIntroEl.textContent = `You have entered ${kilos}kg, which will be equal to:`;
+  divEl.append(outputIntroEl);
+}
+
+function kilosToPounds(kilos) {
+  const poundsVal = kilos * 2.2046;
+  const pKilosEl = document.createElement("p");
+  pKilosEl.textContent = `Pounds(lb): ${poundsVal};`;
+  divEl.append(pKilosEl);
+}
+
+function kilosToGrams(kilos) {
+  const gramsVal = kilos / 0.001;
+  const pGramsEl = document.createElement("p");
+  pGramsEl.textContent = `Grams(g): ${gramsVal};`;
+  divEl.append(pGramsEl);
+}
+
+function kilosToOunces(kilos) {
+  const ouncesVal = kilos * 35.274;
+  const pOuncesEl = document.createElement("p");
+  pOuncesEl.textContent = `Ounces(oz): ${ouncesVal};`;
+  divEl.append(pOuncesEl);
+}
+
+//main function for input validation and all outputs==============================
+function validInput(input) {
+  if (input.length < 1) {
+    divEl.className = "error";
+    divEl.textContent = "Please enter the value";
+    return divEl;
+  }
+  if (input != Number(input)) {
+    divEl.className = "error";
+    divEl.textContent = "Please enter numbers only";
+    return divEl;
+  } else {
+    if ((divEl.className = "error")) {
+      divEl.classList.toggle("error");
+    }
+    outputIntro(input);
+    kilosToPounds(input);
+    kilosToGrams(input);
+    kilosToOunces(input);
+  }
+}
