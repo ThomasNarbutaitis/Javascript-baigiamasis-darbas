6a3027eea27fd3e75b14a550daaa4fe538332d92
Tomas <tomas_narba@yahoo.com>
2022-04-05 12:41:25 +0300

    ketvirta uzd cards

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,4 +9,37 @@ Pastaba: Informacija apie automobilį (brand) (jo kortelė) bei turi turėti
 bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const endpoint = "cars.json";
+const outputEl = document.getElementById("output");
+
+window.addEventListener("load", () => {
+  getCars();
+});
+
+async function getCars() {
+  try {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    console.log("data ===", data);
+    genAllCars(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function genOneCard(brand, models) {
+  const cardEl = document.createElement("div");
+  cardEl.className = "card";
+  const cardTitle = document.createElement("h2");
+  const cardText = document.createElement("p");
+  cardTitle.textContent = `${brand}`;
+  cardText.textContent = `${models}`;
+  cardEl.append(cardTitle, cardText);
+  outputEl.append(cardEl);
+}
+
+function genAllCars(ObjAarray) {
+  return ObjAarray.forEach((obj) => {
+    genOneCard(obj.brand, obj.models);
+  });
+}
