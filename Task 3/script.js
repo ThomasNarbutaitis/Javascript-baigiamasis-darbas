c0f149b8a4499adb7c7fd58bd18345c203b3b5ba
Tomas <tomas_narba@yahoo.com>
2022-04-05 10:02:02 +0300

    trecia uz

--- Task 3/script.js
+++ Task 3/script.js
@@ -10,4 +10,47 @@ Paspaudus mygtuką "Show users":
 Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const endpoint = "https://api.github.com/users1";
+const btnEl = document.getElementById("btn");
+const outputEl = document.getElementById("output");
+const messageEl = document.getElementById("message");
+
+btnEl.addEventListener("click", getUsers);
+
+//functions==============================================
+
+//main function
+async function getUsers() {
+  try {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+
+    changeMessage(response);
+    generateCards(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//hides message or shows error if fails to fetch
+function changeMessage(resp) {
+  if (resp.ok) {
+    messageEl.className = "hide";
+  } else {
+    messageEl.textContent = "Unexpected error. Please try again later...";
+  }
+}
+
+//single card generation
+function createSingleCard(name, pic) {
+  const cardEl = document.createElement("figure");
+  cardEl.className = "fig";
+  cardEl.innerHTML = `<img src="${pic}" alt="user image" />
+  <figcaption>username: ${name}</figcaption>`;
+  outputEl.append(cardEl);
+}
+
+//generates all cards
+function generateCards(array) {
+  array.map((obj) => createSingleCard(obj.login, obj.avatar_url));
+}
